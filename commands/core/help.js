7024a254045a84e74ced49e6154316795ff43314
JaquetaGamer <trb5128@gmail.com>
2022-08-12 18:26:55 +0100

    Fixed help pannel 👨🏿‍🦲
    Literally it 💀

--- commands/core/help.js
+++ commands/core/help.js
@@ -13,7 +13,7 @@ module.exports = {
         .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ size: 2048, dynamic: true }) })
         .setDescription('Help pannel for the bot!')
         .addFields([ { name: `Commands (Core) - 3`, value: "| `invite` | `ping` | `help` |" } ])
-        .addFields([ { name: `Commands (Music) - 20`, value: "| `back` | `clear` | `controller` | `filter` | `jump` | `loop` |\n| `nowplaying` | `pause` | `play` | `playnext` | `queue` | `resume` |\n| `search` | `seek` | `shuffle` | `skip` | `stop` | `volume` |" } ])
+        .addFields([ { name: `Commands (Music) - 20`, value: "| `back` | `clear` | `controller` | `disconnect` | `filter` |\n| `jump` | `loop` | `nowplaying` | `pause` | `play` |\n| `playnext` | `queue` | `remove` | `resume` | `search` |\n| `seek` | `shuffle` | `skip` | `stop` | `volume` |" } ])
         .setTimestamp()
         .setFooter({ text: '❤️', iconURL: inter.member.avatarURL({ dynamic: true })});
 
